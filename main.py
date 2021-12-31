ce841513cd5c06ff06a4d6caeaddb5749eaa277e
SirBrahms <sirbrahms16@gmail.com>
2021-12-31 16:55:41 +0100

    adding the first files

---
+++ main.py
@@ -0,0 +1,26 @@
+import os
+
+#Defining global variables
+arg = ""
+paramList = []
+
+#function to distinguish the first element in a line and seperate it
+def formStrings(fulllist):
+    global arg
+    global paramList
+    arg = fulllist[0]
+    fulllist.remove(arg)
+    paramList = fulllist
+
+#opening the file
+file1 = input("Enter file to open: ")
+openfile = open(file1, "r")
+lines = openfile.readlines()
+
+#format each line properly
+for e in lines:
+    stripstring = e.strip()
+    splitstring = stripstring.split(" ")
+    formStrings(splitstring)
+    print(arg)
+    print(paramList)
