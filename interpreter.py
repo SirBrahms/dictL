2a1fc99ae5b6c488c97cd0eb6f77c7185f91c688
SirBrahms <sirbrahms16@gmail.com>
2022-01-01 14:35:23 +0100

    code refactor & added commands

--- interpreter.py
+++ interpreter.py
@@ -4,6 +4,7 @@ import os
 arg = ""
 paramList = []
 pos = 1 #position/"index" in the dictionary
+pushvar = 0
 
 #defining the starting dictionary
 maindict = {}
@@ -30,8 +31,17 @@ def dictL_down():
     pos -= 1
 
 def dictL_print():
-    global pos
-    global maindict
+    global pos, maindict, paramList
+    fullprint = ""
+    if(paramList[0] != ""):
+        print(maindict[str(paramList[0])])
+        return
+    elif("!lit" in paramList):
+        for e in paramList:
+            if(e != "!lit"):
+                fullprint += e + " "
+        print(fullprint)
+        return
     print(maindict[str(pos)])
 
 def dictL_reset():
@@ -39,8 +49,7 @@ def dictL_reset():
     maindict = {}
 
 def dictL_jumpToIndex():
-    global pos
-    global maindict
+    global pos, maindict, paramList
     jumpval = paramList[0]
     if(int(jumpval) <= len(maindict)):
         pos = jumpval
@@ -50,6 +59,21 @@ def dictL_jumpToIndex():
 def dictL_change():
     global maindict, pos, paramList
     maindict[str(pos)] = paramList[0]
+
+def dictL_pushIndex():
+    global pos, pushvar
+    pushvar = pos
+
+def dictL_popIndex():
+    global pos, pushvar
+    if(pushvar != 0):
+        pos = pushvar
+        pushvar = 0
+
+def dictL_printAll():
+    global maindict
+    for e in maindict:
+        print(e + " : " + maindict[e])
 
 def checkArg(arg):
     global maindict
@@ -69,6 +93,12 @@ def checkArg(arg):
         dictL_jumpToIndex()
     if(arg.lower() == "chng" and maindict != {}):
         dictL_change()
+    if(arg.lower() == "pushi" and maindict != {}):
+        dictL_pushIndex()
+    if(arg.lower() == "popi" and maindict != {}):
+        dictL_popIndex()
+    if(arg.lower() == "prall"):
+        dictL_printAll()
 
 
 #opening the file
